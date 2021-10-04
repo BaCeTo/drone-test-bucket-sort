4df56f5cc0984291040bb2567bc29464b8b654b8
Vasil Vasilev <v.vasilev@adcash.com>
2021-10-04 17:00:19 +0300

    A commit that breaks tests.

--- tests/BucketSortTest.php
+++ tests/BucketSortTest.php
@@ -29,7 +29,7 @@ class BucketSortTest extends TestCase
     {
         $bucketSort = new BucketSort($this->getBucketSettings());
 
-        $bucketId = $bucketSort->getBucketId(0);
+        $bucketId = $bucketSort->getBucketId(5);
         $this->assertEquals(1, $bucketId);
 
         $bucketId = $bucketSort->getBucketId(1);
